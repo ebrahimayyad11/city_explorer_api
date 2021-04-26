a281e575848cc191cc669630d8c32e9c9e44379f
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-04-26 21:30:09 +0300

    ..

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const { request, response } = require('express');
 const express = require('express');
 const server = express();
+// const cors = cors();
 const PORT = process.env.PORT || 5000;
 server.get('/', (request, response) => {
   response.status(200).json();
   response.send('try another rout');
 });
 
+// server.use(cors());
 
 server.listen(PORT, () => {
   console.log(PORT);
@@ -30,10 +32,45 @@ server.get('/location', (request, response) => {
   response.status(200).send(newLocation);
 });
 
+function getDay (str){
+  let newStr = str.substr(0,2);
+  switch(newStr){
+  case '13' : return 'Mon';
+
+  case '14' : return 'Tue';
+
+  case '15' : return 'Wed';
+
+  case '16' : return 'Thu';
+
+  case '17' : return 'Fri';
+
+  default : return 'Error';
+  }
+}
+
+function reverseDate (str){
+  let newStr;
+  newStr += str[8];
+  newStr += str[9];
+  newStr += str[7];
+  newStr += str[5];
+  newStr += str[6];
+  newStr += str[4];
+  newStr += str[0];
+  newStr += str[1];
+  newStr += str[2];
+  newStr += str[3];
+
+  return newStr;
+}
 
 let Weather = function (obj) {
   this.forecast = obj.weather.description;
-  this.time = obj.valid_date;
+  let date = obj.valid_date;
+  let newDate = reverseDate(date);
+  let day = getDay(newDate);
+  this.time = day+newDate;
 };
 
 server.get('/weather', (request, response) => {
@@ -43,7 +80,6 @@ server.get('/weather', (request, response) => {
     let newWeather = new Weather(item);
     arr.push(newWeather);
   });
-  // console.log(arr);
   response.status(200).send(arr);
 });
 
