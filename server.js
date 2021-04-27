2c5a37717732818b8a7e1fa996bb936be70d1caa
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-04-27 20:39:00 +0300

    doing lab-06

--- server.js
+++ server.js
@@ -32,45 +32,11 @@ server.get('/location', (request, response) => {
   response.status(200).send(newLocation);
 });
 
-function getDay (str){
-  let newStr = str.substr(0,2);
-  switch(newStr){
-  case '13' : return 'Mon';
 
-  case '14' : return 'Tue';
-
-  case '15' : return 'Wed';
-
-  case '16' : return 'Thu';
-
-  case '17' : return 'Fri';
-
-  default : return 'Error';
-  }
-}
-
-function reverseDate (str){
-  let newStr;
-  newStr += str[8];
-  newStr += str[9];
-  newStr += str[7];
-  newStr += str[5];
-  newStr += str[6];
-  newStr += str[4];
-  newStr += str[0];
-  newStr += str[1];
-  newStr += str[2];
-  newStr += str[3];
-
-  return newStr;
-}
 
 let Weather = function (obj) {
   this.forecast = obj.weather.description;
-  let date = obj.valid_date;
-  let newDate = reverseDate(date);
-  let day = getDay(newDate);
-  this.time = day+newDate;
+  this.time = new Date(obj.valid_date).toDateString();
 };
 
 server.get('/weather', (request, response) => {
