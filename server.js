cf3c0e7f7483d775ee717d5ccf4bfa3097d15ed0
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-05-01 15:42:01 +0300

    doing lab 06

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 'use strict';
-require('dotenv').config();
-const { request, response } = require('express');
-const express = require('express');
-const server = express();
-// const cors = cors();
+const express=require('express');
+require ('dotenv').config();
+const cors =require('cors');
+
+
+const server=express();
 const PORT = process.env.PORT || 5000;
-server.get('/', (request, response) => {
-  response.status(200).json();
-  response.send('try another rout');
-});
+server.use(cors());
 
-// server.use(cors());
 
-server.listen(PORT, () => {
-  console.log(PORT);
-});
+
 
 let Location = function (obj) {
   let split = obj[0].display_name.split(' ');
@@ -56,4 +51,10 @@ server.get('*',(req,res)=>{
     resText: 'sorry! this page not found'
   };
   res.status(500).send(errObj);
+});
+
+
+
+server.listen(PORT, () => {
+  console.log(PORT);
 });
